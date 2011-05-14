2b700189fb135d5ae2c3b9e92fe27153441cc8d8
maulana.jaka@gmail.com <maulana.jaka@gmail.com@96809f1f-1833-9599-6d45-8d2596f1d025>
2011-05-14 20:29:29 +0000

    Perubahan nama method pada interface MinimarketService.java @ Issue 10 Perubahan nama method dan argumen pada class MinimarketServiceImpl @ Issue 11 penambahan method testParentDelete di class MinimarketServiceImplTest @ issue 13

--- src/main/java/workshop/minimarket/service/MinimarketService.java
+++ src/main/java/workshop/minimarket/service/MinimarketService.java
@@ -19,12 +19,12 @@ public interface MinimarketService {
     public void simpanGrup(Grup grup);
     public void hapusGrup(Grup grup);
     public List<Grup> cariSemuaGrup();
-    public Grup cariGrupByKodeGrup(String kodeGrup);
+    public Grup cariGrupByKodeGrup(Long kodeGrup);
     
     public void simpanProduk(Produk produk);
     public void hapusProduk(Produk produk);
     public List<Produk> cariSemuaProduk();
-    public Produk cariSemuaProdukByKodeProduk(String kodeProduk);   
+    public Produk cariProdukByKodeProduk(Long kodeProduk);   
     
 //    public void simpanBarang(Barang barang);
 //    public void hapusBarang(Barang barang);
@@ -40,4 +40,5 @@ public interface MinimarketService {
 //    public void hapusPenjualanDetail(PenjualanDetail penjualanDetail);
 //    public List<PenjualanDetail> cariSemuaPenjualanDetail();
 //    public PenjualanDetail cariSemuaPenjualanDetailByNoNota(String noNota);     
+
 }
