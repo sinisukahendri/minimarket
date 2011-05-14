26f974639ee3e840146c2a369370e93107979701
doodenk@gmail.com <doodenk@gmail.com@96809f1f-1833-9599-6d45-8d2596f1d025>
2011-05-14 09:12:25 +0000

    menambahkan Class Grup.java pada package workshop.minimarket.entity

---
+++ trunk/src/main/java/workshop/minimarket/entity/Grup.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop.minimarket.entity;
+
+/**
+ *
+ * @author BangsJack
+ */
+public class Grup {    
+    private String kodeGrup;    
+    private String namaGrup;
+
+    public String getKodeGrup() {
+        return kodeGrup;
+    }
+
+    public void setKodeGrup(String kodeGrup) {
+        this.kodeGrup = kodeGrup;
+    }
+
+    public String getNamaGrup() {
+        return namaGrup;
+    }
+
+    public void setNamaGrup(String namaGrup) {
+        this.namaGrup = namaGrup;
+    }   
+}
