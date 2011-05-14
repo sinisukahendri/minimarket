5ab2e1e68432bfa60ad6d7e9a6b5adc31c4ebbac
doodenk@gmail.com <doodenk@gmail.com@96809f1f-1833-9599-6d45-8d2596f1d025>
2011-05-14 12:01:22 +0000

    membuat service interface - membuat package workshop.minimarket.service -> point 1 - membuat service interface minimarketServise -> point 2 @ Issue 10

---
+++ src/main/java/workshop/minimarket/service/minimarketService.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package workshop.minimarket.service;
+
+import java.util.List;
+import workshop.minimarket.entity.Barang;
+import workshop.minimarket.entity.Grup;
+import workshop.minimarket.entity.Penjualan;
+import workshop.minimarket.entity.PenjualanDetail;
+import workshop.minimarket.entity.Produk;
+
+/**
+ *
+ * @author BangsJack
+ */
+public interface minimarketService {
+    public void simpanGrup(Grup grup);
+    public void hapusGrup(Grup grup);
+    public List<Grup> cariSemuaGrup();
+    public Grup cariSemuaGrupByKodeGrup(String kodeGrup);
+    
+    public void simpanProduk(Produk produk);
+    public void hapusProduk(Produk produk);
+    public List<Produk> cariSemuaProduk();
+    public Produk cariSemuaProdukByKodeProduk(String kodeProduk);   
+    
+//    public void simpanBarang(Barang barang);
+//    public void hapusBarang(Barang barang);
+//    public List<Barang> cariSemuaBarang();
+//    public Barang cariSemuaBarangByKodeBarang(String kodeBarang); 
+//    
+//    public void simpanPenjualan(Penjualan penjualan);
+//    public void hapusPenjualan(Penjualan penjualan);
+//    public List<Penjualan> cariSemuaPenjualan();
+//    public Penjualan cariSemuaPenjualanByNoNota(String noNota); 
+//    
+//    public void simpanPenjualanDetail(PenjualanDetail penjualanDetail);
+//    public void hapusPenjualanDetail(PenjualanDetail penjualanDetail);
+//    public List<PenjualanDetail> cariSemuaPenjualanDetail();
+//    public PenjualanDetail cariSemuaPenjualanDetailByNoNota(String noNota);     
+}
